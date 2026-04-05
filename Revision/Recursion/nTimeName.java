81494eb71b8d12f6ec3f27cd826faab2d730e791
Hari Om Verma <hariohmverma96@gmail.com>
2026-04-05 20:17:25 +0530

    revision/Recursion/nTimeName.java

--- Revision/Recursion/nTimeName.java
+++ Revision/Recursion/nTimeName.java
@@ -1,10 +1,10 @@
 public class nTimeName {
     public static void printNameNTimes(String name, int n) {
         if (n <= 0) {
-            return; // Base case: stop when n is 0 or negative
+            return;
         }
-        System.out.println(name); // Print the name
-        printNameNTimes(name, n - 1); // Recursive call with n decremented by 1
+        System.out.println(name); 
+        printNameNTimes(name, n - 1); 
     }
     public static void main(String[] args) {
         String name = "John";
