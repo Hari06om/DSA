4aede7a8bb020bd03564f855bdb305be697b56bb
Hari Om Verma <hariohmverma96@gmail.com>
2026-05-03 11:00:37 +0530

    new pyramid pattern with numbers instead of stars

--- Revision/Pattern/numberPyramid.java
+++ Revision/Pattern/numberPyramid.java
@@ -7,11 +7,14 @@ public class numberPyramid {
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
         for(int i =0;i<n;i++){
+            // Print leading spaces for centering
+            for(int k=0; k<n-i-1; k++){
+                System.out.print(" ");
+            }
             for(int j=0;j<=i;j++){
                 System.out.print(j+1+" ");  
             }   
             System.out.println();
-
         }
     }
 }
