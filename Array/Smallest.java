f9108a3cbcaf300c5b4472f2f4101818ea2bb873
Hari Om Verma <hariohmverma96@gmail.com>
2026-05-04 19:25:14 +0530

    right angle triangle pattern with numbers

--- Array/Smallest.java
+++ Array/Smallest.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Smallest{
-    public static int smallest(int[] arr){
-        int smll = Integer.MAX_VALUE;
-        for(int i=0; i<arr.length; i++){
-        if(arr[i]<smll){
-            smll = arr[i];
-             }
-        }
-        return smll;
+// public class Smallest{
+//     public static int smallest(int[] arr){
+//         int smll = Integer.MAX_VALUE;
+//         for(int i=0; i<arr.length; i++){
+//         if(arr[i]<smll){
+//             smll = arr[i];
+//              }
+//         }
+//         return smll;
 
-    }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the Size of the array: ");
+//         int n = sc.nextInt();
+//         System.out.print("Enter the elements of the arrays: ");
+//         int[] arr = new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i] = sc.nextInt();  
+//         }
+//         System.out.println("The smallest element in the array is: " + smallest(arr));
+//     }
+// }
+
+public class Smallest {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Size of the array: ");
-        int n = sc.nextInt();
-        System.out.print("Enter the elements of the arrays: ");
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();  
+        int n = 5; 
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = n; i >= 1; i--) {
+            sb.append("*".repeat(i)).append("\n");
         }
-        System.out.println("The smallest element in the array is: " + smallest(arr));
+        System.out.print(sb.toString());
     }
 }
